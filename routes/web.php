3115608c798637c8396df55b8cd54df8209e678c
PeeperInTheSpace <lollof94@gmail.com>
2022-11-04 19:56:34 +0100

    Completed!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'PageController@index');
